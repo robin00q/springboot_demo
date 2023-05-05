346faaa1a08e2e6380d8ced371de1b99b398fecf
sukjune lee <robin00q@naver.com>
2023-05-05 18:28:49 +0900

    add jib.gradle

--- src/main/java/com/example/demo_springboot/TerraformController.java
+++ src/main/java/com/example/demo_springboot/TerraformController.java
@@ -1,5 +1,7 @@
 package com.example.demo_springboot;
 
+import java.net.InetAddress;
+import java.net.UnknownHostException;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,7 +9,8 @@ import org.springframework.web.bind.annotation.RestController;
 public class TerraformController {
 
   @GetMapping("/")
-  public String hello() {
-    return "hello terraform";
+  public String hello() throws UnknownHostException {
+    InetAddress localhost = InetAddress.getLocalHost();
+    return String.format("hello terraform from [%s]", localhost.getHostAddress());
   }
 }
